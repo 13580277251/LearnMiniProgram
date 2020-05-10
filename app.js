4ab9f156fe57df5b19072139259311118712929d
13580277251 <346561501@qq.com>
2020-05-11 02:48:06 +0800

    注册小程序和页面

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // 注册小程序示例
+/* 是一个全局共享的单例对象，所以可以将一些贡献数据放在这里 */
 App({
+
+  globalData:{
+    name:'zjl',
+    age:18
+  },
 
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
@@ -13,6 +19,7 @@ App({
    */
   onShow: function (options) {
     // 1.判断小程序的进入场景
+    // options.scene：场景值ID
     console.log(options.scene);
     switch(options){
       case 1001:
@@ -22,9 +29,9 @@ App({
     }
 
     // 2.获取用户信息，并且获取用户信息之后，将用户的信息传递给服务器
+    /* 但是已经即将废弃这个接口，改用1.button组件-将open-type改成getUserInfo,并且绑定事件去点击，2.使用open-data开放能力组件 */
     wx.getUserInfo({
-      complete: (res) => {console.log(res);
-      },
+      complete: (res) => {console.log(res);},
     })
     
   },
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,18 +1,35 @@
 // pages/home/home.js
+
+// getApp()获取App()产生示例对象
+const app = getApp()
+
+const name = app.globalData.name
+const age = app.globalData.age
+
+// 注册一个page页面
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+   /* -----------------------页面的初始数据----------------- */
   data: {
-
+    world:'zjl',
+    list:[]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+ 
+    /* --------------------------生命周期函数--监听页面加载------------------------ */
+ 
   onLoad: function (options) {
-
+    wx.request({
+      url: 'http://152.136.185.210:8000/api/n3/recommend',
+      success:(res) => {
+        console.log(res);
+        console.log(this);
+        
+        const data = res.data.data.list
+        this.setData({
+          list:data
+        })
+      }
+    })
   },
 
   /**
@@ -42,25 +59,32 @@ Page({
   onUnload: function () {
 
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  /* ----------------------1.监听wxml中的相关的一些事件--------------------------- */
+    // 监听事件的点击，接受监听事件
+    handleGeetYserInfo(event){
+      console.log(event);
+    },
+    handleViewClick(){
+      console.log('监听点击事件');
+    },
+    /* ------------------------2.监听其他事件------------------------------- */
+    // 监听页面的滚动
+    onPageScroll(obj){
+      console.log(obj);
+    },
+    // 上拉加载更多
+    onReachBottom(){
+      console.log('页面滚动到底部');
+    },
+    // 下拉刷新事件
+    onPullDownRefresh(){
+      console.log('下拉刷新');
+      
+    }
 })
